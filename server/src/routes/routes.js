84dacc520064d81ea88fcc7ccf02b1a70a095c5b
Petrick Lim <petrick.lim@gmail.com>
2018-10-22 09:45:16 +0800

    added login comp

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,5 +2,6 @@ const authController = require('../controllers/authController')
 const authControllerPolicy = require('../policies/authControllerPolicies.js')
 
 module.exports = (app) => {
+  app.post('/login', authController.login)
   app.post('/register', authControllerPolicy.register, authController.register)
 }
